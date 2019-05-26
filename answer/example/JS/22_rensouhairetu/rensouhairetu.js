e0fd3b42888003240e8460b6584e914aa22e221a
fumihumi <t.fum1hum@gmail.com>
2019-05-26 17:42:13 +0900

    fix 22

--- answer/example/JS/22_rensouhairetu/rensouhairetu.js
+++ answer/example/JS/22_rensouhairetu/rensouhairetu.js
@@ -4,6 +4,7 @@ const rensouhairetu = {
     'size': '100'
 };
 rensouhairetu.count = '5';
+
 Object.keys(rensouhairetu).forEach(function (key) {
-    console.log('keyは: ', key, 'valueは: ', rensouhairetu[key]);
+    console.log( `key: ${ key }, value: ${ rensouhairetu[ key ] }` );
 });
