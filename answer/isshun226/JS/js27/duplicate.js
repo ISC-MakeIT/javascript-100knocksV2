2e66378b194263670be715855a3e39f45a96696e
isshun226 <51151631+isshun226@users.noreply.github.com>
2019-06-11 23:30:35 +0900

    js27修正

--- answer/isshun226/JS/js27/duplicate.js
+++ answer/isshun226/JS/js27/duplicate.js
@@ -1,5 +1,4 @@
 let array = new Array(25);
-let array2 = new Array(75);
 for (let i = 0; i < array.length; i++) {
     let random = Math.floor(Math.random() * (75 - 1 + 1) + 1);
     if (array.includes(random) == false) {
