95c5aaee44ef6c285f3128e9c09717f7fd2dd12a
isshun226 <51151631+isshun226@users.noreply.github.com>
2019-06-28 13:46:02 +0900

    重複ファイル削除

--- answer/isshun226/JS/js19/typeof.js
+++
@@ -1,8 +0,0 @@
-const num = 1;
-console.log(typeof (num));
-const zero = 0;
-console.log(typeof (zero));
-const str = '1';
-console.log(typeof (str));
-const zs = '0';
-console.log(typeof (zs));
--- answer/isshun226/JS/js22/rensouhairetu.js
+++
@@ -1,5 +0,0 @@
-let array = { color: 'red', size: '100' };
-array.count = '5';
-for (let key in array) {
-    console.log('keyは:' + key + ' ' + 'valueは:' + array[key]);
-}
