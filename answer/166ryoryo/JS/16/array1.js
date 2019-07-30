48ff73f5928b81ab8bdd312159ad7b0ebda09eee
166ryoryo <bjrh1290166@gn.iwasaki.ac.jp>
2019-07-30 09:03:03 +0900

    js16

---
+++ answer/166ryoryo/JS/16/array1.js
@@ -0,0 +1,8 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-unused-vars */
+const ary = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+// eslint-disable-next-line func-names
+ary.forEach((vars) => {
+// eslint-disable-next-line no-console
+  console.log(vars);
+});
