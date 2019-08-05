83a6ef6cbc2b5232441510f81f86ba089c835f36
166ryoryo <bjrh1290166@gn.iwasaki.ac.jp>
2019-08-05 09:02:02 +0900

    js18

---
+++ answer/166ryoryo/JS/18/array3.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const ary = [];
+// eslint-disable-next-line no-unused-vars
+const num = 1;
+// eslint-disable-next-line no-unused-vars
+const obj = {};
+
+alert(Array.isArray(ary));
+alert(Array.isArray(num));
+alert(Array.isArray(obj));
