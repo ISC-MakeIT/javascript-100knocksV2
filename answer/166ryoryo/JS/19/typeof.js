a23f18834860f1f06db37c96ddf5f22002dde888
166ryoryo <bjrh1290166@gn.iwasaki.ac.jp>
2019-08-05 15:39:52 +0900

    js19

---
+++ answer/166ryoryo/JS/19/typeof.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const num = 1;
+// eslint-disable-next-line no-unused-vars
+const zero = 0;
+// eslint-disable-next-line no-unused-vars
+const str = '1';
+// eslint-disable-next-line no-unused-vars
+const zs = '0';
+
+alert(typeof (num));
+alert(typeof (zero));
+alert(typeof (str));
+alert(typeof (zs));
