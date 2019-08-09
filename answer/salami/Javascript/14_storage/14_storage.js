4d81b63fa3a56e30a9e36758769414af5ab0d964
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2019-08-09 18:25:21 +0900

    JS_14

---
+++ answer/salami/Javascript/14_storage/14_storage.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  let a = window.sessionStorage.getItem('key');
+  const b = document.getElementById('outputsave');
+  if (a == null) {
+    a = 0;
+  } else {
+    b.value = a;
+  }
+};
+
+const store = () => {
+  const a = document.getElementById('outputsave');
+  const b = a.value;
+  window.sessionStorage.setItem('key', b);
+}
