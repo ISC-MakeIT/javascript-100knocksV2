514f6a1eefb956916a4e2814b05b13a1fd8fb230
shinooooo <bjys1290150@gn.iwasaki.ac.jp>
2019-08-10 19:55:27 +0900

    修正

--- answer/shinooooo/JS/14_strage/strage.js
+++ answer/shinooooo/JS/14_strage/strage.js
@@ -1,9 +1,8 @@
 const save = () => {
-  let num = document.getElementById('Language').selectedIndex;
-  localStorage.setItem('select_num',num);
+  const num = document.getElementById('Language').selectedIndex;
+  localStorage.setItem('select_num', num);
 };
 
 const load = () => {
   document.getElementById('Language').selectedIndex = localStorage.getItem('select_num');
-};
-
+}
