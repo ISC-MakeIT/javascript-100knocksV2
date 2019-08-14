99643e3dc60fbb56107e1b64defd47455eff8992
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-08-14 17:32:34 +0900

    コメント位置の変更

--- answer/masaya2019/JS/05_randomBgColor/randomBgColor.js
+++ answer/masaya2019/JS/05_randomBgColor/randomBgColor.js
@@ -1,9 +1,7 @@
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
+  // カラーコード(#16進数)作成
   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   document.getElementById("BG").style.backgroundColor = randomColor;
 } );
-
-// カラーコード(#16進数)作成
-
