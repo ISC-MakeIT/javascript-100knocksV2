2c8364f47851a6cc3ea4f77eed082fe4b1c3f2cd
masaya2019 <bjmk1290313@gn.iwasaki.ac.jp>
2019-08-21 10:50:45 +0900

    let→constに変更

--- answer/masaya2019/JS/03_toBin/toBin.js
+++ answer/masaya2019/JS/03_toBin/toBin.js
@@ -2,7 +2,7 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(e) {
   e.preventDefault();
-  let textForm = document.getElementById("textForm").value;
+  const textForm = document.getElementById("textForm").value;
   let num = parseInt(textForm);
   let result = num.toString(2);
   let resultForm = document.getElementById("resultForm");
