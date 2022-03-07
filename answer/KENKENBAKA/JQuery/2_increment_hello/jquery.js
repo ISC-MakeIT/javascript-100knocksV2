7b31613390550e63166e94719a1766b823fa4764
kentyisapen <kenkenpoco@gmail.com>
2022-03-08 01:52:05 +0900

    refactor jquery 2

--- answer/KENKENBAKA/JQuery/2_increment_hello/jquery.js
+++ answer/KENKENBAKA/JQuery/2_increment_hello/jquery.js
@@ -1,8 +1,15 @@
-num = 1
+num = 0
 
 window.onload = function () {
   $('#button1').on("click", function () {
-    $("#hello-area").append($("<div>").text("Hello " + num))
+    if (num == 0) {
+      let newDivElement = $('div')
+      newDivElement.text("Hello " + num)
+      newDivElement.attr("class", "hello")
+      $("#hello-area").append(newDivElement)
+    } else {
+      $(".hello").text("Hello " + num)
+    }
     num += 1
   })
 }
